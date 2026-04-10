be4f5a9c8eed6fb489c5d0ee550a672e1b424563
taconoodle <giannisvlassis1@gmail.com>
2026-04-11 00:10:05 +0300

    Added a S3 error handler. Added unit tests for create_object, upload_to_bucket, download_from_bucket, delete_from_bucket, close, copy_file_part methods

--- Manager/storage.py
+++ Manager/storage.py
@@ -3,6 +3,7 @@ from botocore.exceptions import *
 from botocore.client import Config
 import json
 from itertools import groupby
+from contextlib import contextmanager
 
 
 RUSTFS_USERNAME = 'admin'
@@ -16,19 +17,11 @@ class S3Storage:
     
     def __init__(self):
         self.conn = None
-        
-    
-    def init_s3(self):
+
+    @contextmanager
+    def _handle_errors(self):
         try:
-            self.conn = boto3.client(
-                's3',
-                endpoint_url=RUSTFS_URL,
-                aws_access_key_id=RUSTFS_USERNAME,
-                aws_secret_access_key=RUSTFS_PASSWORD,
-                config=Config(signature_version='s3v4'), # The version of the signatures the authenticates AWS requests. RustFS wants s3v4
-                region_name='us-east-1' # Apparently RustFS expects us to use us-east-1
-            )
-            return self.conn
+            yield
         except EndpointConnectionError as e:
             raise S3ConnectionError('Could not reach S3 endpoint')
         except ClientError as e:
@@ -38,103 +31,96 @@ class S3Storage:
                     raise S3ConnectionError('Invalid username')
                 case 'SignatureDoesNotMatch':
                     raise S3ConnectionError('Invalid password')
+                case 'BucketAlreadyExists':
+                    raise S3ConnectionError('Bucket already exists')
+                case 'InvalidBucketName':
+                    raise S3ConnectionError('Invalid bucket name')
+                case 'TooManyBuckets':
+                    raise S3ConnectionError('Too many buckets')
+                case 'NoSuchBucket':
+                    raise S3ConnectionError('Bucket does not exist')
+                case 'NoSuchKey':
+                    raise S3ConnectionError('Key does not exist')
                 case _:
                     raise S3ConnectionError(f'Unknown error: {e}')
-    
-    
+
+    def init_s3(self):
+        with self._handle_errors():
+            self.conn = boto3.client(
+                's3',
+                endpoint_url=RUSTFS_URL,
+                aws_access_key_id=RUSTFS_USERNAME,
+                aws_secret_access_key=RUSTFS_PASSWORD,
+                config=Config(signature_version='s3v4'), # The version of the signatures the authenticates AWS requests. RustFS wants s3v4
+                region_name='us-east-1' # Apparently RustFS expects us to use us-east-1
+            )
+            return self.conn
+
     def create_bucket(self, bucket_name):
-        try:
-            self.conn.create_bucket(Bucket=bucket_name)
-            print(f'Bucket {bucket_name} created.')
-        except self.conn.exceptions.BucketAlreadyExists:
-            print(f'Bucket {bucket_name} already exists.')
-        except ClientError as e:
-            print(f'Unexpected error occured: {e}')
-        return
+        with self._handle_errors():
+            response = self.conn.create_bucket(Bucket=bucket_name)
+            return response
     
     def create_object(self, bucket, key, body):
-        self.conn.put_object(
-            Bucket=bucket,
-            Key=key,
-            Body=body
-        )
+        with self._handle_errors():
+            response = self.conn.put_object(
+                Bucket=bucket,
+                Key=key,
+                Body=body
+            )
+            return response
 
     def upload_to_bucket(self, bucket_name, filename, key=None):
-        if key is None:
-            key = filename
-        self.conn.upload_file(Filename=filename, Bucket=bucket_name, Key=key)
-
+        with self._handle_errors():
+            if key is None:
+                key = filename
+            self.conn.upload_file(Bucket=bucket_name, Key=key, Filename=filename)
 
     def download_from_bucket(self, bucket_name, key, filename=None):
-        if filename is None:
-            filename = key
-        self.conn.download_file(Key=key, Bucket=bucket_name, Filename=filename)
-
+        with self._handle_errors():
+            if filename is None:
+                filename = key
+            self.conn.download_file(Bucket=bucket_name, Key=key, Filename=filename)
 
     def delete_from_bucket(self, bucket_name, key):
-        self.conn.delete_object(Bucket=bucket_name, Key=key)
-
+        with self._handle_errors():
+            response = self.conn.delete_object(Bucket=bucket_name, Key=key)
+            return response
 
     def close(self):
         if self.conn is not None:
             self.conn.close()
-            
-            
-    def test_s3(self):
-        bucket_name = 'test-bucket'
 
-        self.create_bucket(bucket_name)
-        print(f'Created bucket {bucket_name}')
+    def copy_file_part(self, upload_id, src_bucket, src_key, dst_bucket, dst_key, byte_amount_mb=64, byte_offset=0, part_number=1):
+        if byte_amount_mb <= 0:
+            raise S3ConnectionError('Byte amount must be at least 0')
+        if byte_offset < 0:
+            raise S3ConnectionError('Byte offset must be greater than 0')
+        if part_number < 1 or part_number > 10000:
+            raise S3ConnectionError('Invalid part number. Part number must be between 1 and 10000')
 
-        buckets = self.conn.list_buckets()
-        print('Buckets found:')
-        for bucket in buckets['Buckets']:
-            print(f'\t{bucket['Name']}')
+        with self._handle_errors():
+            file_size = self.conn.head_object(Bucket=src_bucket, Key=src_key)['ContentLength']
+            byte_amount = byte_amount_mb * (1024 ** 2)
 
-        self.conn.upload_file(Filename='test.txt', Bucket=bucket_name, Key='test.txt')
-        print('File "test.txt" uploaded')
+            first_byte = byte_offset
+            last_byte = min(byte_offset + byte_amount - 1, file_size - 1)
+            byte_range = f'bytes={first_byte}-{last_byte}'
 
-        print(f'Now printing objects in {bucket_name}:')
-        objects_in_bucket = self.conn.list_objects_v2(Bucket=bucket_name)
-        for obj in objects_in_bucket.get('Contents', []):
-            print(f'\t{obj['Key']} - ({obj['Size']} bytes)')
+            copy_source = {
+                'Bucket': src_bucket,
+                'Key': src_key
+            }
 
-        self.conn.download_file(Bucket=bucket_name, Key='test.txt', Filename='download-test.txt')
-        print('File should be have been downloaded as "download-test.txt"')
-
-        self.conn.delete_object(Bucket=bucket_name, Key='test.txt')
-        print('Object deleted.')
-
-        self.conn.delete_bucket(Bucket=bucket_name)
-        print('Bucket deleted.')
-
-
-    def copy_file_part(self, upload_id, src_bucket, src_key, dst_bucket, dst_key=None, byte_amount_mb=64, byte_offset=0, part_number=1):
-        if dst_key is None:
-            dst_key = src_key
-
-        file_size = self.conn.head_objet(Bucket=src_bucket, Key=src_key)['ContentLength']
-        byte_amount = byte_amount_mb * (1024 ** 2)
-
-        first_byte = byte_offset
-        last_byte = min(byte_offset + byte_amount - 1, file_size - 1)
-        byte_range = f'bytes={first_byte}-{last_byte}'
-
-        copy_source = {
-            'Bucket': src_bucket,
-            'Key': src_key
-        }
-
-        response = self.conn.upload_part_copy(
-            UploadId=upload_id,
-            Bucket=dst_bucket,
-            Key=dst_key,
-            PartNumber=part_number, # PartNumber needs to be 1 to 10000 (Source: RTFM)
-            CopySource=copy_source,
-            CopySourceRange=byte_range
-        )
-        return response
-
+            response = self.conn.upload_part_copy(
+                UploadId=upload_id,
+                Bucket=dst_bucket,
+                Key=dst_key,
+                PartNumber=part_number, # PartNumber needs to be 1 to 10000 (Source: RTFM)
+                CopySource=copy_source,
+                CopySourceRange=byte_range
+            )
+            return response
 
     def stream_file_pairs(self, bucket, key):
         response = self.conn.get_object(
@@ -233,10 +219,3 @@ class S3Storage:
             UploadId=upload_id,
             MultipartUpload={'Parts': parts}
         )
-        
-if __name__ == "__main__":
-    s3 = S3Storage()
-    s3.init_s3_storage()
-    
-    s3.test_s3()
-    s3.close()
--- test/Manager/test_storage.py
+++ test/Manager/test_storage.py
@@ -2,13 +2,24 @@ from Manager.storage import S3Storage, S3ConnectionError
 
 import pytest
 from unittest.mock import patch, MagicMock, ANY
-from botocore.exceptions import ClientError, EndpointConnectionError
+from botocore.exceptions import *
+
+TEST_BUCKET_NAME = 'test-bucket'
+TEST_KEY_NAME = 'test-key'
+TEST_FILE_NAME = 'test-file'
+TEST_BODY = 'test-body'
+TEST_ENDPOINT_URL = 'http://test-url:9000'
+TEST_USERNAME = 'admin'
+TEST_PASSWORD = 'admin'
+TEST_UPLOAD_ID = 'test-upload-id'
+TEST_BYTE_AMOUNT_MB = 100
+TEST_BYTE_OFFSET = 10
 
 class TestS3Storage:
 
-    @patch('Manager.storage.RUSTFS_URL', 'http://test-url:9000')
-    @patch('Manager.storage.RUSTFS_USERNAME', 'admin')
-    @patch('Manager.storage.RUSTFS_PASSWORD', 'admin')
+    @patch('Manager.storage.RUSTFS_URL', TEST_ENDPOINT_URL)
+    @patch('Manager.storage.RUSTFS_USERNAME', TEST_USERNAME)
+    @patch('Manager.storage.RUSTFS_PASSWORD', TEST_PASSWORD)
     @patch('Manager.storage.boto3.client')
     def test_init_s3_calls_boto3_with_correct_args(self, mock_boto_client):
         mock_conn = MagicMock()
@@ -21,9 +32,9 @@ class TestS3Storage:
 
         mock_boto_client.assert_called_once_with(
             's3',
-            endpoint_url='http://test-url:9000',
-            aws_access_key_id='admin',
-            aws_secret_access_key='admin',
+            endpoint_url=TEST_ENDPOINT_URL,
+            aws_access_key_id=TEST_USERNAME,
+            aws_secret_access_key=TEST_PASSWORD,
             config=ANY,
             region_name='us-east-1'
         )
@@ -60,7 +71,7 @@ class TestS3Storage:
     @patch('Manager.storage.boto3.client')
     def test_init_s3_raises_on_invalid_endpoint_url(self, mock_boto_client):
         mock_boto_client.side_effect = EndpointConnectionError(
-            endpoint_url='http://test-url:9000'
+            endpoint_url=TEST_ENDPOINT_URL
         )
 
         s3 = S3Storage()
@@ -80,4 +91,405 @@ class TestS3Storage:
         with pytest.raises(S3ConnectionError) as except_info:
             s3.init_s3()
 
-        assert f"Unknown error" in str(except_info.value)
+        assert "Unknown error" in str(except_info.value)
+
+    @pytest.fixture
+    def mock_conn(self):
+        return MagicMock()
+
+    @pytest.fixture
+    def s3(self, mock_conn):
+        with patch('Manager.storage.boto3.client') as mock_boto:
+            mock_boto.return_value = mock_conn
+            s3 = S3Storage()
+            s3.init_s3()
+            yield s3
+
+    def test_create_bucket_success(self, s3, mock_conn):
+        mock_conn.create_bucket.return_value = {
+            'Location': f'/{TEST_BUCKET_NAME}'
+        }
+        result = s3.create_bucket(TEST_BUCKET_NAME)
+
+        mock_conn.create_bucket.assert_called_once_with(Bucket=TEST_BUCKET_NAME)
+        assert result['Location'] == f'/{TEST_BUCKET_NAME}'
+
+    def test_create_bucket_already_exists(self, s3, mock_conn):
+        mock_conn.create_bucket.side_effect = ClientError(
+            error_response={'Error': {'Code': 'BucketAlreadyExists'}},
+            operation_name='TestBucketCreate'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.create_bucket(TEST_BUCKET_NAME)
+        assert 'Bucket already exists' in str(except_info.value)
+
+    def test_create_bucket_invalid_name(self, s3, mock_conn):
+        mock_conn.create_bucket.side_effect = ClientError(
+            error_response={'Error': {'Code': 'InvalidBucketName'}},
+            operation_name='TestBucketCreate'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.create_bucket(TEST_BUCKET_NAME)
+        assert 'Invalid bucket name' in str(except_info.value)
+
+    def test_create_bucket_when_too_many_exist(self, s3, mock_conn):
+        mock_conn.create_bucket.side_effect = ClientError(
+            error_response={'Error': {'Code': 'TooManyBuckets'}},
+            operation_name='TestBucketCreate'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.create_bucket(TEST_BUCKET_NAME)
+        assert 'Too many buckets' in str(except_info.value)
+
+    def test_create_bucket_raises_unknown_error(self, s3, mock_conn):
+        mock_conn.create_bucket.side_effect = ClientError(
+            error_response={'Error': {'Code': 'UnknownError'}},
+            operation_name='TestBucketCreate'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.create_bucket(TEST_BUCKET_NAME)
+        assert 'Unknown error' in str(except_info.value)
+
+    def test_create_object_success(self, s3, mock_conn):
+        mock_conn.put_object.return_value = {
+            'ETag': 'fake-etag'
+        }
+        result = s3.create_object(TEST_BUCKET_NAME, TEST_KEY_NAME, TEST_BODY)
+
+        mock_conn.put_object.assert_called_once_with(
+            Bucket=TEST_BUCKET_NAME,
+            Key=TEST_KEY_NAME,
+            Body=TEST_BODY
+        )
+        assert result['ETag'] == 'fake-etag'
+
+    def test_create_object_bucket_does_not_exist(self, s3, mock_conn):
+        mock_conn.put_object.side_effect = ClientError(
+            error_response={'Error': {'Code': 'NoSuchBucket'}},
+            operation_name='TestCreateObject'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.create_object(TEST_BUCKET_NAME, TEST_KEY_NAME, TEST_BODY)
+        assert 'Bucket does not exist' in str(except_info.value)
+
+    def test_create_object_invalid_bucket_name(self, s3, mock_conn):
+        mock_conn.put_object.side_effect = ClientError(
+            error_response={'Error': {'Code': 'InvalidBucketName'}},
+            operation_name='TestCreateObject'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.create_object(TEST_BUCKET_NAME, TEST_KEY_NAME, TEST_BODY)
+        assert 'Invalid bucket name' in str(except_info.value)
+
+    def test_create_object_endpoint_failure(self, s3, mock_conn):
+        mock_conn.put_object.side_effect = EndpointConnectionError(
+            endpoint_url='http://test-url:9000'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.create_object(TEST_BUCKET_NAME, TEST_KEY_NAME, TEST_BODY)
+        assert 'Could not reach S3 endpoint' in str(except_info.value)
+
+    def test_create_object_unknown_error(self, s3, mock_conn):
+        mock_conn.put_object.side_effect = ClientError(
+            error_response={'Error': {'Code': 'UnknownError'}},
+            operation_name='TestCreateObject'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.create_object(TEST_BUCKET_NAME, TEST_KEY_NAME, TEST_BODY)
+        assert 'Unknown error' in str(except_info.value)
+
+    def test_upload_to_bucket_success(self, s3, mock_conn):
+        s3.upload_to_bucket(TEST_BUCKET_NAME, TEST_FILE_NAME, TEST_KEY_NAME)
+
+        mock_conn.upload_file.assert_called_once_with(
+            Bucket=TEST_BUCKET_NAME,
+            Key=TEST_KEY_NAME,
+            Filename=TEST_FILE_NAME
+        )
+
+    def test_upload_to_bucket_with_no_keyname(self, s3, mock_conn):
+        s3.upload_to_bucket(TEST_BUCKET_NAME, TEST_FILE_NAME)
+
+        mock_conn.upload_file.assert_called_once_with(
+            Bucket=TEST_BUCKET_NAME,
+            Key=TEST_FILE_NAME,
+            Filename=TEST_FILE_NAME
+        )
+
+    def test_upload_to_bucket_does_not_exist(self, s3, mock_conn):
+        mock_conn.upload_file.side_effect = ClientError(
+            error_response={'Error': {'Code': 'NoSuchBucket'}},
+            operation_name='TestUploadToBucket'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.upload_to_bucket(TEST_BUCKET_NAME, TEST_FILE_NAME, TEST_KEY_NAME)
+        assert 'Bucket does not exist' in str(except_info.value)
+
+    def test_upload_to_bucket_invalid_bucket_name(self, s3, mock_conn):
+        mock_conn.upload_file.side_effect = ClientError(
+            error_response={'Error': {'Code': 'InvalidBucketName'}},
+            operation_name='TestUploadToBucket'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.upload_to_bucket(TEST_BUCKET_NAME, TEST_FILE_NAME, TEST_KEY_NAME)
+        assert 'Invalid bucket name' in str(except_info.value)
+
+    def test_upload_to_bucket_endpoint_failure(self, s3, mock_conn):
+        mock_conn.upload_file.side_effect = EndpointConnectionError(
+            endpoint_url='http://test-url:9000'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.upload_to_bucket(TEST_BUCKET_NAME, TEST_FILE_NAME, TEST_KEY_NAME)
+        assert 'Could not reach S3 endpoint' in str(except_info.value)
+
+    def test_upload_to_bucket_unknown_error(self, s3, mock_conn):
+        mock_conn.upload_file.side_effect = ClientError(
+            error_response={'Error': {'Code': 'UnknownError'}},
+            operation_name='TestUploadToBucket'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.upload_to_bucket(TEST_BUCKET_NAME, TEST_FILE_NAME, TEST_KEY_NAME)
+        assert 'Unknown error' in str(except_info.value)
+
+    def test_download_from_bucket_success(self, s3, mock_conn):
+        s3.download_from_bucket(TEST_BUCKET_NAME, TEST_KEY_NAME, TEST_FILE_NAME)
+
+        mock_conn.download_file.assert_called_once_with(
+            Key=TEST_KEY_NAME,
+            Bucket=TEST_BUCKET_NAME,
+            Filename=TEST_FILE_NAME
+        )
+
+    def test_download_from_bucket_with_no_filename(self, s3, mock_conn):
+        s3.download_from_bucket(TEST_BUCKET_NAME, TEST_KEY_NAME)
+
+        mock_conn.download_file.assert_called_once_with(
+            Key=TEST_KEY_NAME,
+            Bucket=TEST_BUCKET_NAME,
+            Filename=TEST_KEY_NAME
+        )
+
+    def test_download_from_bucket_bucket_does_not_exist(self, s3, mock_conn):
+        mock_conn.download_file.side_effect = ClientError(
+            error_response={'Error': {'Code': 'NoSuchBucket'}},
+            operation_name='TestDownloadFromBucket'
+        )
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.download_from_bucket(TEST_BUCKET_NAME, TEST_KEY_NAME)
+        assert 'Bucket does not exist' in str(except_info.value)
+
+    def test_download_from_bucket_key_does_not_exist(self, s3, mock_conn):
+        mock_conn.download_file.side_effect = ClientError(
+            error_response={'Error': {'Code': 'NoSuchKey'}},
+            operation_name='TestDownloadFromBucket'
+        )
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.download_from_bucket(TEST_BUCKET_NAME, TEST_KEY_NAME)
+        assert 'Key does not exist' in str(except_info.value)
+
+    def test_download_from_bucket_invalid_bucket_name(self, s3, mock_conn):
+        mock_conn.download_file.side_effect = ClientError(
+            error_response={'Error': {'Code': 'InvalidBucketName'}},
+            operation_name='TestDownloadFromBucket'
+        )
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.download_from_bucket(TEST_BUCKET_NAME, TEST_KEY_NAME)
+        assert 'Invalid bucket name' in str(except_info.value)
+
+    def test_download_from_bucket_endpoint_failure(self, s3, mock_conn):
+        mock_conn.download_file.side_effect = EndpointConnectionError(
+            endpoint_url=TEST_ENDPOINT_URL
+        )
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.download_from_bucket(TEST_BUCKET_NAME, TEST_KEY_NAME)
+        assert 'Could not reach S3 endpoint' in str(except_info.value)
+
+    def test_download_from_bucket_unknown_error(self, s3, mock_conn):
+        mock_conn.download_file.side_effect = ClientError(
+            error_response={'Error': {'Code': 'UnknownError'}},
+            operation_name='TestDownloadFromBucket'
+        )
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.download_from_bucket(TEST_BUCKET_NAME, TEST_KEY_NAME)
+        assert 'Unknown error' in str(except_info.value)
+
+    # I'LL STOP WRITING TESTS FOR EVERYTHING FROM NOW ON, OUR HANDLER WORKS
+    # I'LL WRITE HAPPY PATH AND MORE SPECIFIC TESTS
+
+    def test_delete_from_bucket_success(self, s3, mock_conn):
+        mock_conn.delete_object.return_value = {'ResponseMetadata': {}}
+        result = s3.delete_from_bucket(TEST_BUCKET_NAME, TEST_KEY_NAME)
+
+        mock_conn.delete_object.assert_called_once_with(
+            Bucket=TEST_BUCKET_NAME,
+            Key=TEST_KEY_NAME
+        )
+        assert result is not None
+
+    def test_close_success(self, s3, mock_conn):
+        s3.close()
+        s3.conn = None
+        s3.close()
+
+    def test_copy_file_part_success(self, s3, mock_conn):
+        mock_conn.head_object.return_value = {'ContentLength': 200 * (1024 ** 2)}
+        mock_conn.upload_part_copy.return_value = {
+            'CopyPartResult': {
+                'ETag': 'test-etag'
+            }
+        }
+        test_byte_range = f'bytes={TEST_BYTE_OFFSET}-{TEST_BYTE_AMOUNT_MB * (1024 ** 2) + TEST_BYTE_OFFSET - 1}'
+        result = s3.copy_file_part(
+            TEST_UPLOAD_ID,
+            'test-src-bucket',
+            'test-src-key',
+            'test-dst-bucket',
+            'test-dst-key',
+            TEST_BYTE_AMOUNT_MB,
+            TEST_BYTE_OFFSET,
+            2
+        )
+
+        mock_conn.upload_part_copy.assert_called_once_with(
+            UploadId=TEST_UPLOAD_ID,
+            Bucket='test-dst-bucket',
+            Key='test-dst-key',
+            PartNumber=2,
+            CopySource={'Bucket': 'test-src-bucket', 'Key': 'test-src-key'},
+            CopySourceRange=test_byte_range
+        )
+        assert result == {
+            'CopyPartResult': {
+                'ETag': 'test-etag'
+            }
+        }
+
+    def test_copy_file_part_without_optional_args(self, s3, mock_conn):
+        mock_conn.head_object.return_value = {'ContentLength': 200 * (1024 ** 2)}
+        mock_conn.upload_part_copy.return_value = {
+            'CopyPartResult': {
+                'ETag': 'test-etag'
+            }
+        }
+        test_byte_range = f'bytes={0}-{64 * (1024 ** 2) - 1}'
+        result = s3.copy_file_part(
+            TEST_UPLOAD_ID,
+            'test-src-bucket',
+            'test-src-key',
+            'test-dst-bucket',
+            'test-dst-key',
+        )
+
+        mock_conn.upload_part_copy.assert_called_once_with(
+            UploadId=TEST_UPLOAD_ID,
+            Bucket='test-dst-bucket',
+            Key='test-dst-key',
+            PartNumber=1,
+            CopySource={'Bucket': 'test-src-bucket', 'Key': 'test-src-key'},
+            CopySourceRange=test_byte_range
+        )
+        assert result == {
+            'CopyPartResult': {
+                'ETag': 'test-etag'
+            }
+        }
+
+    def test_copy_file_part_range_smaller_than_file(self, s3, mock_conn):
+        mock_conn.head_object.return_value = {'ContentLength': 10 * (1024 ** 2)}
+        mock_conn.upload_part_copy.return_value = {
+            'CopyPartResult': {
+                'ETag': 'test-etag'
+            }
+        }
+        test_byte_range = f'bytes={TEST_BYTE_OFFSET}-{10 * (1024 ** 2) - 1}'
+        result = s3.copy_file_part(
+            TEST_UPLOAD_ID,
+            'test-src-bucket',
+            'test-src-key',
+            'test-dst-bucket',
+            'test-dst-key',
+            TEST_BYTE_AMOUNT_MB,
+            TEST_BYTE_OFFSET,
+            2
+        )
+
+        mock_conn.upload_part_copy.assert_called_once_with(
+            UploadId=TEST_UPLOAD_ID,
+            Bucket='test-dst-bucket',
+            Key='test-dst-key',
+            PartNumber=2,
+            CopySource={'Bucket': 'test-src-bucket', 'Key': 'test-src-key'},
+            CopySourceRange=test_byte_range
+        )
+        assert result == {
+            'CopyPartResult': {
+                'ETag': 'test-etag'
+            }
+        }
+
+    def test_copy_file_part_invalid_args(self, s3, mock_conn):
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.copy_file_part(
+                TEST_UPLOAD_ID,
+                'test-src-bucket',
+                'test-src-key',
+                'test-dst-bucket',
+                'test-dst-key',
+                0,
+                TEST_BYTE_OFFSET,
+                2
+            )
+        assert 'Byte amount must be at least 0' in str(except_info.value)
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.copy_file_part(
+                TEST_UPLOAD_ID,
+                'test-src-bucket',
+                'test-src-key',
+                'test-dst-bucket',
+                'test-dst-key',
+                TEST_BYTE_AMOUNT_MB,
+                -1,
+                2
+            )
+        assert 'Byte offset must be greater than 0' in str(except_info.value)
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.copy_file_part(
+                TEST_UPLOAD_ID,
+                'test-src-bucket',
+                'test-src-key',
+                'test-dst-bucket',
+                'test-dst-key',
+                TEST_BYTE_AMOUNT_MB,
+                TEST_BYTE_OFFSET,
+                20000
+            )
+        assert 'Invalid part number. Part number must be between 1 and 10000' in str(except_info.value)
+
+        with pytest.raises(S3ConnectionError) as except_info:
+            s3.copy_file_part(
+                TEST_UPLOAD_ID,
+                'test-src-bucket',
+                'test-src-key',
+                'test-dst-bucket',
+                'test-dst-key',
+                TEST_BYTE_AMOUNT_MB,
+                TEST_BYTE_OFFSET,
+                -1
+            )
+        assert 'Invalid part number. Part number must be between 1 and 10000' in str(except_info.value)
+
